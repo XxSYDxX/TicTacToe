95499265f9050a1274aae61823a07c8a2e13d525
Was <93242673+was07@users.noreply.github.com>
2022-02-13 14:37:15 +0600

    Update UI.py
    renamed markup flag and markup class to unidentical names

--- UI.py
+++ UI.py
@@ -7,7 +7,7 @@ table = [0]*9
 ptype = 'X'
 etype = 'O'
 
-class markups:
+class Markups:
     bcyan = '\033[96m\033[1m' if markups else ''
     bred = '\033[91m\033[1m' if markups else ''
     byellow = '\033[93m\033[1m' if markups else ''
@@ -18,16 +18,18 @@ def ptab(table, matched_line = []):
     row = ''
     for i in range(9):
         if table[i] == 0: row += ' ' + str(i+1)
-        if table[i] == 1: row += (markups.byellow if i in matched_line else markups.bcyan) + ' ' + ptype + markups.normal
-        if table[i] == 2: row += (markups.byellow if i in matched_line else markups.bred) + ' ' + etype + markups.normal
+        if table[i] == 1: row += (Markups.byellow if i in matched_line else Markups.bcyan) + ' ' + ptype + Markups.normal
+        if table[i] == 2: row += (Markups.byellow if i in matched_line else Markups.bred) + ' ' + etype + Markups.normal
+    
         if i in (2, 5, 8): 
             print(row)
             row = ''
 
 
 def get_move():
-    move = input("Your move: [1-9/q] " + markups.bcyan)
-    print(markups.normal, end='')
+    table = [0]*9
+    move = input("Your move: [1-9/q] " + Markups.bcyan)
+    print(Markups.normal, end='')
     if move.lower().strip() == 'q':
         exit()
     if move.isnumeric() and int(move) in range(1, 10):
@@ -48,11 +50,11 @@ def game():
     e = Engine()
     
     first = 1   # Who goes first (1: Player, 2: Engine)
-    if input("Which X (first) or O (second)? [X/O] " + markups.bcyan).strip().upper() == 'O': 
+    if input("Which X (first) or O (second)? [X/O] " + Markups.bcyan).strip().upper() == 'O': 
         first = 2
         etype = 'X'
         ptype = 'O'
-    print(markups.normal, end='')
+    print(Markups.normal, end='')
         
     if first == 1:
         ptab(table)
@@ -67,20 +69,20 @@ def game():
             ptab(table)
             table[get_move()] = 1
             emove = e.engine_move(table)
-            if emove == 9:
+            if emove == -10:
                 continue
             table[emove] = 2
         else:
             if finished == 1:
                 ptab(table, e.checked_indices(table))
-                print(markups.byellow + "You won. Dammit, how did you do that?")
+                print(Markups.byellow + "You won. Dammit, how did you do that?")
             elif finished == 2:
                 ptab(table, e.checked_indices(table))
-                print(markups.byellow + "Engine wins.")
+                print(Markups.byellow + "Engine wins.")
             else:
                 ptab(table)
-                print(markups.byellow + "Tie.")
-            print(markups.normal, end='')
+                print(Markups.byellow + "Tie.")
+            print(Markups.normal, end='')
             # Ask user if they want to play again
             if input("Play again? [Y/n] ").strip().lower() == 'y':
                 game()
